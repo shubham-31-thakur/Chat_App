9891897dd342bd0013241fe5750c70adcfbbdff6
Shubham Thakur <84236393+shubham-31-thakur@users.noreply.github.com>
2024-04-20 02:57:51 +0530

    Console.WriteLine modified

--- Chat_server/main.cpp
+++ Chat_server/main.cpp
@@ -2,6 +2,8 @@
 #include<WinSock2.h>
 #include<ws2tcpip.h>
 #include<tchar.h>
+#include<thread>
+#include<vector>
 using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
@@ -27,6 +29,37 @@ bool Intialize() {
 	WSADATA data;
 	return WSAStartup(MAKEWORD(2, 2), &data) == 0;
 
+}
+
+void InteractWithClient(SOCKET clientSocket, vector<SOCKET> clients){
+	//send rec client
+
+	cout << "Client connected" << endl;
+
+	char buffer[4096];
+	while (1) {
+
+		int bytesrecv = recv(clientSocket, buffer, sizeof(buffer), 0);
+		if (bytesrecv <= 0) {
+			cout << "client disconnected" << endl;
+			break;
+		}
+		string message(buffer, bytesrecv);
+		cout << "message from client : " << message << endl;
+
+		for (auto client : clients) {
+			if(client != clientSocket){
+			send(client, message.c_str(), message.length(), 0);
+			}
+		}
+	}
+
+	auto it = find(clients.begin(), clients.end(), clientSocket);
+	if (it != clients.end()){
+		clients.erase(it);
+	}
+
+	closesocket(clientSocket);
 }
 int main() {
 	if (!Intialize()) {
@@ -74,21 +107,19 @@ int main() {
 	
 	} //2nd para takes the value that can the requests in a queue, SOMAXCONN is sin integer
 	cout << "server has started listening on port :" << port << endl;
-
-	//Accept
-	SOCKET clientsocket = accept(listenSocket, nullptr, nullptr); // Client address as 2nd parameter, and print address as 2rd para, but not required so write null
-		if(clientsocket == INVALID_SOCKET) {
+	vector<SOCKET> clients;
+	
+	while (1) {
+		//Accept
+		SOCKET clientsocket = accept(listenSocket, nullptr, nullptr); // Client address as 2nd parameter, and print address as 2rd para, but not required so write null
+		if (clientsocket == INVALID_SOCKET) {
 			cout << "Invalid Socket" << endl;
 		}
-
-		char buffer[4096];
-
-		int bytesrecv = recv(clientsocket, buffer, sizeof(buffer), 0);
-
-		string message(buffer, bytesrecv);
-		cout << "message from client : " << message << endl;
-		closesocket(clientsocket);
-		closesocket(listenSocket);
+		clients.push_back(clientsocket);
+		thread t1(InteractWithClient, clientsocket, std::ref(clients));
+		t1.detach();
+	}
+	closesocket(listenSocket);
 		
 	WSACleanup();
 	return 0;
